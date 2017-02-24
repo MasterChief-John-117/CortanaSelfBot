c3b9915c4cb5c723985df66f253f7e9c358e0886
MasterChief-John-117 <galenguyer@outlook.com>
2017-02-24 13:15:54 -0800

    Main Bot with Ping

---
+++ CortanaSelfBot/MainBot.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using ParameterType = Discord.Commands.ParameterType;
+
+namespace CortanaSelfBot
+{
+    public class MainBot
+    {
+        public DiscordClient Discord;
+
+        public MainBot()
+
+
+        {
+            Discord = new DiscordClient(x =>
+            {
+                x.LogLevel = LogSeverity.Verbose;
+                x.LogHandler = Log;
+            });
+
+            Discord.UsingCommands(x =>
+            {
+                x.PrefixChar = "c.";
+                x.AllowMentionPrefix = true;
+                x.HelpMode = HelpMode.Public;
+                x.IsSelfBot = true;
+
+            });
+
+            Discord.GetService<CommandService>().CreateCommand("ping")
+            .Description("Just used to check if the bot is up")
+            .Do(async (e) =>
+                {
+                    await e.Message.Edit("Pong!");
+                });
+
+            Discord.ExecuteAndWait(async () =>
+            {
+                await Discord.Connect("MTY5OTE4OTkwMzEzODQ4ODMy.C4pDFQ.Lu9Jk7pRuLQtPrGWf4qMeMptOIM", TokenType.User);
+            });
+        }
+
+        public void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(DateTime.Now + ": " + e.Message);
+        }
+
+    }
+}
