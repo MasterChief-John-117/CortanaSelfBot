b23ff1f945158b617818a70f839f0327f3809a16
MasterChief-John-117 <galenguyer@outlook.com>
2017-02-24 13:22:52 -0800

    Ban and Image uploads

--- CortanaSelfBot/MainBot.cs
+++ CortanaSelfBot/MainBot.cs
@@ -39,6 +39,30 @@ namespace CortanaSelfBot
                     await e.Message.Edit("Pong!");
                 });
 
+            Discord.GetService<CommandService>().CreateCommand("ban")
+            .Description("Uploads the VeraBan image")
+            .Do(async (e) =>
+                {
+                    await e.Message.Delete();
+                    await e.Channel.SendFile("img\\ban.png");
+                });
+            Discord.GetService<CommandService>().CreateCommand("img")
+            .Description("Uploads an image given the name from bot's img folder")
+            .Parameter("name")
+            .Do(async (e) =>
+                {
+                    await e.Message.Delete();
+                    if (File.Exists($"img\\{e.GetArg("name")}"))
+                    {
+                        await e.Channel.SendFile($"img\\{e.GetArg("name")}");
+                    }
+                    else
+                    {
+                        await e.Message.Edit("Sorry, couldn't find any image named " + e.GetArg("name"));
+                        await Task.Delay(TimeSpan.FromSeconds(2f));
+                        await e.Message.Delete();
+                    }
+                });
             Discord.ExecuteAndWait(async () =>
             {
                 await Discord.Connect("MTY5OTE4OTkwMzEzODQ4ODMy.C4pDFQ.Lu9Jk7pRuLQtPrGWf4qMeMptOIM", TokenType.User);
