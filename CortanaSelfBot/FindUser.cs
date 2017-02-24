cb4cdd616811d115d5455ee137fd2aeadae4876e
MasterChief-John-117 <galenguyer@outlook.com>
2017-02-24 13:47:20 -0800

    emoji and away, + finduser method

---
+++ CortanaSelfBot/FindUser.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Discord;
+using Discord.Commands;
+
+namespace CortanaSelfBot
+{
+    public class FindUser
+    {
+        public static Stopwatch Stopwatch = new Stopwatch();
+
+        public static string Global(string name, CommandEventArgs e)
+        {
+            Stopwatch.Restart();
+            IEnumerable<User> users = e.Message.Client.Servers.SelectMany(s => s.Users);
+            int count = 0;
+            string people = "";
+            string rgx = name;
+            LinkedList<ulong> ids = new LinkedList<ulong>();
+            foreach (User user in users)
+            {
+                if (!String.IsNullOrEmpty(user.Nickname) && Regex.IsMatch(user.Nickname, rgx, RegexOptions.IgnoreCase))
+                {
+                    Console.WriteLine(user.Name);
+                    count++;
+                    if (count < 25)
+                    {
+                        people += user.Name + " (" + user.Nickname + ") : " + user.Id.ToString() + "\n";
+                    }
+                    ids.AddFirst(user.Id);
+                }
+                if(Regex.IsMatch(user.Name.ToLower(), rgx, RegexOptions.IgnoreCase) && !ids.Contains(user.Id))
+                {
+                    Console.WriteLine(user.Name);
+                    count++;
+                    people += user.Name + " : " + user.Id.ToString() + "\n";
+                    ids.AddFirst(user.Id);
+                }
+            }
+            if (count > 20)
+            {
+                Stopwatch.Stop();
+                return $"I found `{count}` users! Please use more strict parameters.. :sweat_smile: (Search took `{Stopwatch.ElapsedMilliseconds}`ms)";
+            }
+            else if (count >= 1 && count <= 20)
+            {
+                Stopwatch.Stop();
+                return $"{people}(Search took `{Stopwatch.ElapsedMilliseconds}`ms)";
+            }
+            else
+            {
+                Stopwatch.Stop();
+                return $"It took me `{Stopwatch.ElapsedMilliseconds}`ms to find no one :(";
+            }
+        }
+    }
+}
--- CortanaSelfBot/MainBot.cs
+++ CortanaSelfBot/MainBot.cs
@@ -12,6 +12,8 @@ namespace CortanaSelfBot
     public class MainBot
     {
         public DiscordClient Discord;
+        public String AwayReason;
+        public bool Isaway;
 
         public MainBot()
 
@@ -125,6 +127,72 @@ namespace CortanaSelfBot
                     }
                     else await e.Message.Edit("Sorry, I don't have permissions to delete messages here");
                 });
+            Discord.GetService<CommandService>().CreateCommand("away")
+            .Description("Sets a reason for being away that is returned if the user is mentioned")
+            .Parameter("reason", ParameterType.Unparsed)
+            .Do(async (e) =>
+                {
+                    string reason = e.GetArg("reason");
+
+                    if (!Isaway)
+                    {
+                        Isaway = true;
+                        AwayReason = reason;
+                        await e.Message.Edit($"You have been set to away with the reason `{AwayReason}`");
+                    }
+                    else if (Isaway || String.IsNullOrEmpty(reason))
+                    {
+                        Isaway = false;
+                        AwayReason = "";
+                        await e.Message.Edit("Your away status has been cleared.");
+                    }
+                });
+            Discord.GetService<CommandService>().CreateCommand("emoji")
+            .Description("Converts input text to emoji")
+            .Parameter("text", ParameterType.Unparsed)
+            .Do(async (e) =>
+                {
+                    string message = "";
+                    char[] input = e.GetArg("text").ToCharArray();
+                    foreach (char letter in input)
+                    {
+                        if (char.IsPunctuation(letter))
+                        {
+                            message += $"**{letter}**";
+                        }
+                        else if (char.IsWhiteSpace(letter)) message += "  ";
+                        else if (char.IsLetter(letter))
+                        {
+                            message += $":regional_indicator_{letter.ToString().ToLower()}:";
+                        }
+                        else if (char.IsDigit(letter))
+                        {
+                            message += ToWord(letter);
+                        }
+                    }
+                    await e.Message.Edit(message);
+                });
+            Discord.GetService<CommandService>().CreateCommand("find")
+            .Description("Find a user's ID from name or nick")
+            .Parameter("name", ParameterType.Unparsed)
+            .Do(async (e) =>
+                {
+                    string name = e.GetArg("name");
+                    string message = FindUser.Global(name, e);
+                    await e.Message.Edit(message);
+                });
+
+
+            //For away status
+            Discord.MessageReceived += (async (s, m) =>
+            {
+                if (Isaway && (m.Message.RawText.Contains(Discord.CurrentUser.Mention) ||
+                               m.Message.RawText.Contains(Discord.CurrentUser.NicknameMention)))
+                {
+                    await m.Message.Channel.SendMessage($"Sorry, I'm currently away! Reason: `{AwayReason}`");
+                }
+            });
+
             Discord.ExecuteAndWait(async () =>
             {
                 await Discord.Connect("MTY5OTE4OTkwMzEzODQ4ODMy.C4pDFQ.Lu9Jk7pRuLQtPrGWf4qMeMptOIM", TokenType.User);
@@ -136,5 +204,19 @@ namespace CortanaSelfBot
             Console.WriteLine(DateTime.Now + ": " + e.Message);
         }
 
+        public string ToWord(char letter)
+        {
+            if (letter == '0') return ":zero:";
+            if (letter == '1') return ":one:";
+            if (letter == '2') return ":two:";
+            if (letter == '3') return ":three:";
+            if (letter == '4') return ":four:";
+            if (letter == '5') return ":five:";
+            if (letter == '6') return ":six:";
+            if (letter == '7') return ":seven:";
+            if (letter == '8') return ":eight:";
+            if (letter == '9') return ":nine:";
+            return "";
+        }
     }
 }
