abc4a31d37c2b7db1200786c0f94319ca395765c
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2017-03-17 19:42:19 -0700

    Added code to also scan usernames

--- CortanaSelfBot/FindUser.cs
+++ CortanaSelfBot/FindUser.cs
@@ -1,60 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text.RegularExpressions;
 using Discord;
 using Discord.Commands;
-using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace CortanaSelfBot
 {
-    public class FindUser
+  public class FindUser
+  {
+    public static Stopwatch Stopwatch = new Stopwatch();
+    public static Dictionary<ulong, string> allUs = new Dictionary<ulong, string>();
+
+    public static string Global(string name, CommandEventArgs e)
     {
-        public static Stopwatch Stopwatch = new Stopwatch();
-        public static Dictionary<ulong, string> allUs = new Dictionary<ulong, string>();
-
-        public static string Global(string name, CommandEventArgs e)
+      FindUser.Stopwatch.Restart();
+      IEnumerable<User> users = (IEnumerable<User>) e.Message.Client.Servers.SelectMany<Server, User>((Func<Server, IEnumerable<User>>) (s => s.Users)).OrderBy<User, ulong>((Func<User, ulong>) (s => s.Id));
+      int num = 0;
+      string str = "";
+      LinkedList<string> linkedList1 = new LinkedList<string>();
+      string pattern = name;
+      LinkedList<ulong> linkedList2 = new LinkedList<ulong>();
+      foreach (User user in users)
+      {
+        if (!string.IsNullOrEmpty(user.Nickname) && Regex.IsMatch(user.Nickname, pattern, RegexOptions.IgnoreCase))
         {
-
-            Stopwatch.Restart();
-            IEnumerable<User> users = e.Message.Client.Servers.SelectMany(s => s.Users).OrderBy(s => s.Id);
-            int count = 0;
-            string people = "";
-            LinkedList<string> names = new LinkedList<string>();
-            string rgx = name;
-            LinkedList<ulong> ids = new LinkedList<ulong>();
-            foreach (User user in users)
-            {
-                if (!String.IsNullOrEmpty(user.Nickname) && Regex.IsMatch(user.Nickname, rgx, RegexOptions.IgnoreCase))
-                {
-                    Console.WriteLine(user.Name);
-                    count++;
-                    if (count < 25 && !names.Contains(user.Name))
-                    {
-                        names.AddFirst(user.Name);
-                        people += user.Name + " (" + user.Nickname + ") : `" + user.Id.ToString() + "`\n";
-                    }
-                    ids.AddFirst(user.Id);
-                }
-            }
-            if (count > 20)
-            {
-                Stopwatch.Stop();
-                return
-                    $"I found `{count}` users! Please use more strict parameters.. :sweat_smile: (Search took `{Stopwatch.ElapsedMilliseconds}`ms)";
-            }
-            else if (count >= 1 && count <= 20)
-            {
-                Stopwatch.Stop();
-                return $"{people}(Search took `{Stopwatch.ElapsedMilliseconds}`ms)";
-            }
-            else
-            {
-                Stopwatch.Stop();
-                return $"It took me `{Stopwatch.ElapsedMilliseconds}`ms to find no one :(";
-            }
+          Console.WriteLine(user.Name);
+          ++num;
+          if (num < 25 && !linkedList1.Contains(user.Name))
+          {
+            linkedList1.AddFirst(user.Name);
+            str = str + user.Name + " (" + user.Nickname + ") : `" + user.Id.ToString() + "`\n";
+          }
+          linkedList2.AddFirst(user.Id);
         }
+        if (!string.IsNullOrEmpty(user.Name) && Regex.IsMatch(user.Name, pattern, RegexOptions.IgnoreCase))
+        {
+          Console.WriteLine(user.Name);
+          ++num;
+          if (num < 25 && !linkedList1.Contains(user.Name))
+          {
+            linkedList1.AddFirst(user.Name);
+            str = str + user.Name + ": `" + user.Id.ToString() + "`\n";
+          }
+          linkedList2.AddFirst(user.Id);
+        }
+      }
+      if (num > 20)
+      {
+        FindUser.Stopwatch.Stop();
+        return string.Format("I found `{0}` users! Please use more strict parameters.. :sweat_smile: (Search took `{1}`ms)", (object) num, (object) FindUser.Stopwatch.ElapsedMilliseconds);
+      }
+      if (num >= 1 && num <= 20)
+      {
+        FindUser.Stopwatch.Stop();
+        return string.Format("{0}(Search took `{1}`ms)", (object) str, (object) FindUser.Stopwatch.ElapsedMilliseconds);
+      }
+      FindUser.Stopwatch.Stop();
+      return string.Format("It took me `{0}`ms to find no one :(", (object) FindUser.Stopwatch.ElapsedMilliseconds);
     }
+  }
 }
