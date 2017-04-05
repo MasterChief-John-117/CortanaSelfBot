39bcaf2b2ada0755e63c66fc4497ae47a180ba60
MasterChief-John-117 <galenguyer@outlook.com>
2017-04-04 19:31:58 -0700

    added readme code

--- CortanaSelfBot/Program.cs
+++ CortanaSelfBot/Program.cs
@@ -11,22 +11,55 @@ namespace CortanaSelfBot
     {
         public static void Main(string[] args)
         {
-            if (!File.Exists("notes.json"))
+            if (File.Exists("readme.txt"))
             {
-                File.Create("notes.json");
+                Console.WriteLine(File.ReadAllText("readme.txt"));
+                string str = Console.ReadLine();
+                if (str.ToLower() != "i agree")
+                {
+                    Application.Exit();
+                }
+                else
+                {
+                    File.Delete("readme.txt");
+                    try
+                    {
+                        var mainBot = new MainBot();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.StackTrace);
+                        MessageBox.Show(ex.Message);
+                        Process.Start("CortanaSelfBot.exe");
+                        Application.Exit();
+                    }
+                }
             }
-            try
+            else
             {
-                var mainBot = new MainBot();
+                try
+                {
+                    var mainBot = new MainBot();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.StackTrace);
+                    MessageBox.Show(ex.Message);
+                    Process.Start("CortanaSelfBot.exe");
+                    Application.Exit();
+                }
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.StackTrace);
-                MessageBox.Show(ex.Message);
-                Process.Start("CortanaSelfBot.exe");
-                Application.Exit();
-            }
+
 
         }
     }
 }
+
+
+/*
+This self-bot was made by MasterChief_John-117#1911 and may not be distributed without his written consent.
+Source code can be found at https://github.com/MasterChief-John-117/CortanaSelfBot. To make code edits, fork the repository and make a branch with a descriptive name, then make a push request to be reviewed.
+
+Use of this bot indicates that you are aware of all risks and Terms Of Service put forth by Discord. The maker of this bot is not responsible for any consequenses that may occur from the use of this bot. To indicate that you agree with the above terms, type "I Agree" in the form below.
+(This message will only show on the first run of this bot)
+*/
